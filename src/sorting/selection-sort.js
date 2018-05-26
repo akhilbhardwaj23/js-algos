87d79284602be22688840c856e0a935d82ea34b3
Neha Justa <nehajusta@Nehas-MacBook-Pro.local>
2018-05-26 13:06:39 -0400

    selection sort

--- src/sorting/selection-sort.js
+++ src/sorting/selection-sort.js
@@ -1,5 +1,33 @@
-const selectionSort =  () => {
-    return 1;
+const selectionSort =  (arr) => {
+
+    if(!arr || arr.length === 0){
+        return;  
+     }
+
+     for(let i= 0; i < arr.length ; i++)
+     {
+         let minIndex = i;
+        let minsofar = arr[i];
+
+         for(let j= i; j < arr.length -1 ;j++)
+         {
+            if(arr[j+1] < arr[minIndex])
+            {
+                minIndex = j+1;
+            }
+         } 
+        exchange(arr, i , minIndex);
+
+    }
+
+    return arr;
+}
+
+
+const exchange = (arr, a, b) =>{
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 }
 
 export default selectionSort;
--- src/sorting/sorting.test.js
+++ src/sorting/sorting.test.js
@@ -1,5 +1,13 @@
 import selectionSort from './selection-sort';
 
-test('selection sort', () => {
-    expect(selectionSort()).toBe(1);
+test('selection sort with distinct keys works', () => {
+  const ex1 = [4,5,3,1,2,9,10,8,7,6]
+     expect(selectionSort(ex1)).toEqual([1,2,3,4,5,6,7,8,9,10]);
   });
+
+  test('selection sort with some equal keys works', () => {
+    const ex1 = [4,5,3,1,2,9,10,8,7,6,1,1]
+       expect(selectionSort(ex1)).toEqual([1,1,1,2,3,4,5,6,7,8,9,10]);
+    });
+
+  
